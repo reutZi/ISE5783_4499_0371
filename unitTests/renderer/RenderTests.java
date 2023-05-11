f2ee041088c7a6f1cf482f33d4f88eee1df19a0c
רעות צפלביץ <tzipilev@g.jct.ac.il>
2023-05-11 11:14:38 +0300

    Stage 5 finished

--- unitTests/renderer/RenderTests.java
+++ unitTests/renderer/RenderTests.java
@@ -147,28 +147,37 @@ public class RenderTests {
     * }
     **/
 
+   /** Tests the rendering of a scene from a JSON file using Gson library. */
    @Test
    public void basicRenderJson() {
 
+      // Create a new Gson instance
       Gson gson = new Gson();
 
       try {
+         // Read the JSON file using a FileReader
          FileReader reader = new FileReader("GSON");
          JsonElement json = JsonParser.parseReader(reader);
          JsonObject jsonObject = json.getAsJsonObject();
 
+         // Create a new Scene object and set its background and ambient light properties
          Scene scene = new Scene(jsonObject.get("name").getAsString())
                  .setBackground(gson.fromJson(jsonObject.get("background"), Color.class))
                  .setAmbientLight(gson.fromJson(jsonObject.get("ambientLight"), AmbientLight.class));
 
+         // Get the geometries section of the JSON file, if it exists
          JsonElement jsonGeometries = jsonObject.get("geometries");
 
          if (jsonGeometries != null) {
+            // If the geometries section exists, get the list of geometries
             JsonArray jsonGeometriesList = jsonGeometries.getAsJsonObject().get("geometriesList").getAsJsonArray();
             Geometries geometries = new Geometries();
+
+            // Iterate over each geometry object in the list
             for (JsonElement jsonGeometry : jsonGeometriesList) {
                JsonObject jsonShape = jsonGeometry.getAsJsonObject();
 
+               // If the geometry is a triangle, create a new Triangle object and add it to the Geometries list
                if (jsonShape.has("vertices")) {
 
                   JsonArray jsonVertices = jsonShape.get("vertices").getAsJsonArray();
@@ -179,6 +188,7 @@ public class RenderTests {
 
                   geometries.add(new Triangle(p1, p2, p3));
 
+                  // If the geometry is a sphere, create a new Sphere object and add it to the Geometries list
                } else if (jsonShape.has("radius")) {
 
                   JsonObject jsonCenter = jsonShape.get("center").getAsJsonObject();
@@ -189,9 +199,11 @@ public class RenderTests {
                   geometries.add(new Sphere(radius, center));
                }
             }
+            // Set the Scene's Geometries property to the Geometries list
             scene.setGeometries(geometries);
          }
 
+         // Create a new Camera object with the given parameters, render the image, print the grid, and write to an image file
             Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0))
                     .setVPDistance(100)
                     .setVPSize(500, 500)
@@ -202,16 +214,25 @@ public class RenderTests {
             camera.printGrid(100, new Color(YELLOW));
             camera.writeToImage();
       }
+      // Catch a FileNotFoundException and throw a RuntimeException
       catch(FileNotFoundException e){
          throw new RuntimeException();
       }
    }
 
+   /**
+    * Creates a new Point object from a JsonElement that represents a point in 3D space.
+    * @param jsonPoint the JsonElement that represents the point in JSON format
+    * @return a new Point object with the x, y, and z coordinates from the JsonElement
+    */
       private static Point createPoint(JsonElement jsonPoint) {
+
          JsonObject jsonPointXyz = jsonPoint.getAsJsonObject().get("xyz").getAsJsonObject();
+
          double x = jsonPointXyz.get("d1").getAsDouble();
          double y = jsonPointXyz.get("d2").getAsDouble();
          double z = jsonPointXyz.get("d3").getAsDouble();
+
          return new Point(x,y,z);
       }
 }
